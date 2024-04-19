d2750b839798d09d2000a8827d799ba0e0e07dcc
SHA-inc-unity <zzz20042013@gmail.com>
2024-04-19 16:56:30 +0300

    Ломаная 9я работа

--- ArtemSHloma9_1/app/src/main/java/com/example/artemshloma9_1/MainActivity.java
+++ ArtemSHloma9_1/app/src/main/java/com/example/artemshloma9_1/MainActivity.java
@@ -1,7 +1,7 @@
-package com.example.artemshloma9;
+package com.example.artemshloma9_1;
 
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -14,12 +14,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import android.os.Environment;
-import android.util.Log;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -42,52 +36,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean isExternalStorageReadable() {
-        return false;
-    }
-
-    private void createFileLocal(String fileName, String fileContent){
-        FileOutputStream fos;
-        try {
-            fos = openFileOutput(fileName, MODE_PRIVATE);
-            fos.write(fileContent.getBytes());
-            fos.close();
-
-            Toast.makeText(MainActivity.this, "Файл создан", Toast.LENGTH_LONG).show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void createFileGlobal(String fileName, String fileContent){
-        Log.i("Test", "PrePreA");
-        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), fileName);
-
-        Log.i("Test", file.toString());
-        try (FileOutputStream fos = new FileOutputStream(file)) {
-            Log.i("Test", "A");
-            fos.write(fileContent.getBytes());
-
-            Log.i("Test", "B");
-            Toast.makeText(MainActivity.this, "Файл создан глобально", Toast.LENGTH_LONG).show();
-
-            Log.i("Test", "C");
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.i("Test", e.toString());
-        }
-    }
-
-    public void createFile(View view) {
-        String fileName = editTextFileName.getText().toString();
-        String fileContent = editTextFileContent.getText().toString();
-        fileContent += "\n";
-        if (isExternalStorageReadable()) {
-            Log.i("File Type", "Global");
-            createFileGlobal(fileName, fileContent);
-        }else{
-            Log.i("File Type", "Local");
-            createFileLocal(fileName, fileContent);
-        }
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
     }
 
     private void readFileLocal(String fileName){
@@ -111,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
     private void readFileGlobal(String fileName){
         FileInputStream fis;
         try {
+
             File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), fileName);
 
             fis = new FileInputStream(file);
@@ -125,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
             exportTextFileContent.setText("Файл не найден");
 
             e.printStackTrace();
+            Log.e("Test", e.toString());
         }
 
     }
@@ -133,53 +85,12 @@ public class MainActivity extends AppCompatActivity {
         String fileName = editTextFileName.getText().toString();
         if (isExternalStorageReadable()) {
             Log.i("File Type", "Global");
+            Toast.makeText(MainActivity.this, "Глобально", Toast.LENGTH_LONG).show();
             readFileGlobal(fileName);
         }else{
             Log.i("File Type", "Local");
+            Toast.makeText(MainActivity.this, "Локально", Toast.LENGTH_LONG).show();
             readFileLocal(fileName);
-        }
-    }
-
-    public void appendFile(View view){
-        String fileName = editTextFileName.getText().toString();
-        if (isExternalStorageReadable()) {
-            Log.i("File Type", "Global");
-            appendFileGlobal(fileName);
-        }else{
-            Log.i("File Type", "Local");
-            appendFileLocal(fileName);
-        }
-    }
-
-    public void appendFileLocal(String fileName) {
-        String fileContent = editTextFileContent.getText().toString();
-        fileContent += "\n";
-
-        FileOutputStream fos;
-        try {
-            fos = openFileOutput(fileName, MODE_APPEND);
-            fos.write(fileContent.getBytes());
-            fos.close();
-
-            Toast.makeText(MainActivity.this, "Информация добавлена в файл", Toast.LENGTH_LONG).show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void appendFileGlobal(String fileName) {
-        String fileContent = editTextFileContent.getText().toString();
-        fileContent += "\n";
-
-        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), fileName);
-        try {
-            FileOutputStream fos = new FileOutputStream(file, true); // true for append mode
-            fos.write(fileContent.getBytes());
-            fos.close();
-
-            Toast.makeText(MainActivity.this, "Информация добавлена в файл глобально", Toast.LENGTH_LONG).show();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
